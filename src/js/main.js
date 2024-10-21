ae787373eba8ca05350700d077464f64ab5b653d
Szymon Bednorz <dsonyyy@gmail.com>
2024-10-21 16:20:55 +0200

    split css files

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import * as points from "./points.js";
 
-import "../css/styles.css";
+import "../css/annotation.css";
+import "../css/article.css";
+import "../css/chart.css";
+import "../css/common.css";
+import "../css/content.css";
 import "../css/labels.css";
 
 function enableLoadingScreen() {
