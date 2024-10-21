c68b9e9b5e11ef5cfeb0380f39ba6caff0e7e1b4
Szymon Bednorz <dsonyyy@gmail.com>
2024-10-21 16:16:12 +0200

    refactro labels css

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import * as points from "./points.js";
 
+import "../css/styles.css";
+import "../css/labels.css";
+
 function enableLoadingScreen() {
   document.getElementById("loading").style.display = "block";
   document.getElementById("chart").style.display = "none";
